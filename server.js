03513230a2074b57b0798f160255286faf14d585
unknown <thalesalves758@gmail.com>
2021-11-14 17:39:35 -0300

    Create a handler for a POST request for /api/fileanalyse route

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+
+  res.json({ name, type, size });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
