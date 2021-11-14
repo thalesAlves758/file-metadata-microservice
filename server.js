ad1ed106f36f70c43f60c50807fdbd6dea432ee8
unknown <thalesalves758@gmail.com>
2021-11-14 17:38:01 -0300

    Import multer module

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+const upload = multer();
+require('dotenv').config();
 
 var app = express();
 
@@ -10,9 +12,6 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
